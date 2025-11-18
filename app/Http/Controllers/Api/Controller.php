e4a74ae8448dc36c4e49a819ed7e0cb2e98f423c
Daisa Luise Monteiro <louise2002yaho@gmail.com>
2025-11-18 17:38:49 +0000

    docker

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller as BaseController;
 
+
 class Controller extends BaseController
 {
     // Toute logique spécifique à l'API peut être placée ici
